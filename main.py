a583b5e78f10085e85c22bdf1be8af68942e6cf3
bmccarson <brian@mccarson.us>
2025-08-03 21:56:29 -0400

    crate player and movement

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    clock = pygame.time.Clock()
+    dt = 0
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
     
     while True:
         for event in pygame.event.get():
@@ -15,8 +26,15 @@ def main():
                 return
         
         screen.fill("black")
+       
+        for item in updateable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
         
         
 
